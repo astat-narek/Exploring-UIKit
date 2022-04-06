a69f116af4a075882aec415c5f2d4e13f9f3a773
Narek Danielian <astat.narek@gmail.com>
2022-04-06 23:54:37 +0300

    add login screen

---
+++ 01 Financier/Financier/Financier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Financier
+//
+//  Created by Narek Danielyan on 06.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoginViewController()
+        
+        return true
+    }
+}
+
+
---
+++ 01 Financier/Financier/Financier/Login/LoginViewController.swift
@@ -0,0 +1,110 @@
+//
+//  LoginViewController.swift
+//  Financier
+//
+//  Created by Narek Danielyan on 06.04.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let loginView = LoginView()
+    let signInButton = UIButton(type: .system)
+    let errorMessageLabel = UILabel()
+    
+    var username: String? {
+        return loginView.usernameTextField.text
+    }
+    
+    var password: String? {
+        return loginView.passwordTextField.text
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.configuration = .filled()
+        signInButton.configuration?.imagePadding = 8 // for indicator spacing
+        signInButton.setTitle("Sign in", for: .normal)
+        signInButton.addTarget(self, action: #selector(signinTapped), for: .primaryActionTriggered)
+        
+        errorMessageLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorMessageLabel.textAlignment = .center
+        errorMessageLabel.textColor = .systemRed
+        errorMessageLabel.numberOfLines = 0
+        errorMessageLabel.text = "Error failure"
+        errorMessageLabel.isHidden = true
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        view.addSubview(signInButton)
+        view.addSubview(errorMessageLabel)
+        
+        // Loging View
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+        
+        // Button
+        NSLayoutConstraint.activate([
+            signInButton.topAnchor.constraint(equalToSystemSpacingBelow: loginView.bottomAnchor, multiplier: 2),
+            signInButton.centerXAnchor.constraint(equalTo: loginView.centerXAnchor),
+            signInButton.widthAnchor.constraint(equalTo: loginView.widthAnchor)
+        ])
+        
+        // Error label
+        
+        NSLayoutConstraint.activate([
+            errorMessageLabel.topAnchor.constraint(equalToSystemSpacingBelow: signInButton.bottomAnchor, multiplier: 2),
+            errorMessageLabel.centerXAnchor.constraint(equalTo: loginView.centerXAnchor),
+            errorMessageLabel.widthAnchor.constraint(equalTo: loginView.widthAnchor)
+        ])
+    }
+}
+
+
+// MARK: - Action
+extension LoginViewController {
+    @objc func signinTapped(sender: UIButton) {
+        errorMessageLabel.isHidden = true
+        signInButton.configuration?.showsActivityIndicator = false
+        login()
+    }
+    
+    private func login() {
+        guard let username = username, let password = password else {
+            assertionFailure("Username / password should never be nil")
+            return
+        }
+        
+        if username.isEmpty || password.isEmpty {
+            configureView(withMessage: "Username / password cannot be blank")
+            return
+        }
+        
+        if username == "Kevin" && password == "Welcome" {
+            signInButton.configuration?.showsActivityIndicator = true
+        } else {
+            configureView(withMessage: "Incorrect username / password")
+        }
+    }
+    
+    private func configureView(withMessage message: String) {
+        errorMessageLabel.isHidden = false
+        errorMessageLabel.text = message
+    }
+}
