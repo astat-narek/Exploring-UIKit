699e2bf7338f0e49b02f25cedb70f38ca6dc7f22
Narek Danielian <astat.narek@gmail.com>
2022-04-07 20:34:47 +0300

    add first onboarding screen

--- 01 Financier/Financier/Financier/AppDelegate.swift
+++ 01 Financier/Financier/Financier/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
 //        window?.rootViewController = LoginViewController()
-        window?.rootViewController = OnboardingContainerViewController()
+        window?.rootViewController = OnvoardingViewController()
 
         return true
     }
---
+++ 01 Financier/Financier/Financier/Onboarding/OnvoardingViewController.swift
@@ -0,0 +1,58 @@
+//
+//  OnvoardingViewController.swift
+//  Financier
+//
+//  Created by Narek Danielyan on 07.04.2022.
+//
+
+import Foundation
+import UIKit
+
+class OnvoardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        style()
+        layout()
+    }
+    
+}
+
+extension OnvoardingViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        // Imgage
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "delorean")
+        
+        // Label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = "Financier is faster, easier to use, and has a brand new look feel that will make you feel like you are back in 1989."
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+            ])
+    }
+}
