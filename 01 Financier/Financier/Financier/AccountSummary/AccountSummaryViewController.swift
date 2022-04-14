92ea29a162eda4625051fba9b152e7aab601b111
Narek Danielian <astat.narek@gmail.com>
2022-04-15 00:18:50 +0300

    tests: add alert controller unit tests

--- 01 Financier/Financier/Financier/AccountSummary/AccountSummaryViewController.swift
+++ 01 Financier/Financier/Financier/AccountSummary/AccountSummaryViewController.swift
@@ -22,6 +22,19 @@ class AccountSummaryViewController: UIViewController {
     var headerView = AccountSummaryHeaderView(frame: .zero)
     let refreshControl = UIRefreshControl()
     
+    
+    //Networking
+    var profileManager: ProfileManagable = ProfileManager()
+    
+    // Error alert
+    lazy var errorAlert: UIAlertController = {
+        let alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        return alert
+    }()
+    
+    
+    
     var isLoaded = false
     
     
@@ -130,12 +143,21 @@ extension AccountSummaryViewController: UITableViewDelegate {
 extension AccountSummaryViewController {
     private func fetchData() {
         let group = DispatchGroup()
-        //testing
+        
+        // Testing - random number selection
         let userId = String(Int.random(in: 1..<4))
         
+        fetchProfile(group: group, userId: userId)
+        fetchAccounts(group: group, userId: userId)
         
+        group.notify(queue: .main) {
+            self.reloadView()
+        }
+    }
+    
+    private func fetchProfile(group: DispatchGroup, userId: String) {
         group.enter()
-        fetchProfile(forUserId: String(userId)) { result in
+        profileManager.fetchProfile(forUserId: userId) { result in
             switch result {
             case .success(let profile):
                 self.profile = profile
@@ -144,10 +166,11 @@ extension AccountSummaryViewController {
             }
             group.leave()
         }
-        
+    }
+    
+    private func fetchAccounts(group: DispatchGroup, userId: String) {
         group.enter()
-        fetchAccounts(forUserId: String(userId)) { result in
-            sleep(1) // delete
+        fetchAccounts(forUserId: userId) { result in
             switch result {
             case .success(let accounts):
                 self.accounts = accounts
@@ -156,17 +179,17 @@ extension AccountSummaryViewController {
             }
             group.leave()
         }
+    }
+    
+    private func reloadView() {
+        self.tableView.refreshControl?.endRefreshing()
         
-        group.notify(queue: .main) {
-            self.tableView.refreshControl?.endRefreshing()
-            guard let profile = self.profile else { return }
-            
-            self.isLoaded = true
-            self.configureTableHeaderView(with: profile)
-            self.configureTableCells(with: self.accounts)
-            self.tableView.reloadData()
-        }
+        guard let profile = self.profile else { return }
         
+        self.isLoaded = true
+        self.configureTableHeaderView(with: profile)
+        self.configureTableCells(with: self.accounts)
+        self.tableView.reloadData()
     }
     
     private func configureTableHeaderView(with profile: Profile) {
@@ -184,7 +207,13 @@ extension AccountSummaryViewController {
         }
     }
     
+    
     private func displayError(_ error: NetworkError) {
+        let titleAndMessage = titleAndMessage(for: error)
+        self.showErrorAlert(title: titleAndMessage.0, message: titleAndMessage.1)
+    }
+    
+    private func titleAndMessage(for error: NetworkError) -> (String, String) {
         let title: String
         let message: String
         switch error {
@@ -195,17 +224,14 @@ extension AccountSummaryViewController {
             title = "Network Error"
             message = "Ensure you are connected to the internet. Please try again."
         }
-        self.showErrorAlert(title: title, message: message)
+        return (title, message)
     }
     
+    
     private func showErrorAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title,
-                                      message: message,
-                                      preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        
-        present(alert, animated: true, completion: nil)
+        errorAlert.title = title
+        errorAlert.message = message
+        present(errorAlert, animated: true, completion: nil)
     }
 }
 
@@ -228,5 +254,17 @@ extension AccountSummaryViewController {
         profile = nil
         accounts = []
         isLoaded = false
+    }
+}
+
+
+// MARK: - Unit testing
+extension AccountSummaryViewController {
+    func titleAndMessageForTesting(for error: NetworkError) -> (String, String) {
+        return titleAndMessage(for: error)
+    }
+    
+    func foreFetchProfile() {
+        fetchProfile(group: DispatchGroup(), userId: "1")
     }
 }
---
+++ 01 Financier/Financier/FinancierUnitTests/AccountSummary/AccountSummaryViewControllerTest.swift
@@ -0,0 +1,70 @@
+//
+//  AccountSummaryViewControllerTest.swift
+//  FinancierUnitTests
+//
+//  Created by Narek Danielyan on 14.04.2022.
+//
+
+import Foundation
+import XCTest
+
+@testable import Financier
+
+class AccountSummaryViewControllerTests: XCTestCase {
+    var vc: AccountSummaryViewController!
+    var mockManager: MockProfileManager!
+    
+    
+    
+    class MockProfileManager: ProfileManagable {
+        var profile: Profile?
+        var error: NetworkError?
+        
+        func fetchProfile(forUserId userId: String, completion: @escaping (Result<Profile, NetworkError>) -> Void) {
+            if error != nil {
+                completion(.failure(error!))
+                return
+            }
+            profile = Profile(id: "1", firstName: "FirstName", lastName: "LastName")
+            completion(.success(profile!))
+        }
+    }
+    
+    override func setUp() {
+        super.setUp()
+        vc = AccountSummaryViewController()
+        // vc.loadViewIfNeeded()
+        
+        mockManager = MockProfileManager()
+        vc.profileManager = mockManager
+    }
+    
+    
+    func testTitleAndMessageForServerError() throws {
+        let titleAndMessage = vc.titleAndMessageForTesting(for: .serverError)
+        XCTAssertEqual("Server Error", titleAndMessage.0)
+        XCTAssertEqual("We could not process your request. Please try again.", titleAndMessage.1)
+    }
+    
+    func testTitleAndMessageForNetworkError() throws {
+        let titleAndMessage = vc.titleAndMessageForTesting(for: .decodingError)
+        XCTAssertEqual("Network Error", titleAndMessage.0)
+        XCTAssertEqual("Ensure you are connected to the internet. Please try again.", titleAndMessage.1)
+    }
+    
+    func testAlertForServerError() throws {
+        mockManager.error = NetworkError.serverError
+        vc.foreFetchProfile()
+        
+        XCTAssertEqual("Server Error", vc.errorAlert.title)
+        XCTAssertEqual("We could not process your request. Please try again.", vc.errorAlert.message)
+    }
+    
+    func testAlertForNetworkError() throws {
+        mockManager.error = NetworkError.decodingError
+        vc.foreFetchProfile()
+        
+        XCTAssertEqual("Network Error", vc.errorAlert.title)
+        XCTAssertEqual("Ensure you are connected to the internet. Please try again.", vc.errorAlert.message)
+    }
+}
