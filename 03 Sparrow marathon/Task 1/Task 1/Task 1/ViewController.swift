4cd04c925470595d1837905609a24a94b834fa7f
Narek Danielian <astat.narek@gmail.com>
2023-03-01 23:24:57 +0300

    Task 1 Done: added gradient and auto layout

--- 03 Sparrow marathon/Task 1/Task 1/Task 1/ViewController.swift
+++ 03 Sparrow marathon/Task 1/Task 1/Task 1/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let rectView = gradientView(colors: [UIColor.red.cgColor,UIColor.blue.cgColor])
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        style()
+        layout()
     }
-
-
 }
 
+extension ViewController {
+    
+    func style() {
+        view.backgroundColor = .systemBackground
+        
+        rectView.translatesAutoresizingMaskIntoConstraints = false
+        rectView.layer.cornerRadius = 20
+        rectView.layer.shadowColor = UIColor.black.cgColor
+        rectView.layer.shadowOpacity = 0.5
+        rectView.layer.shadowOffset = .zero
+        rectView.layer.shadowRadius = 10
+        rectView.clipsToBounds = true
+    }
+    
+    func layout() {
+        view.addSubview(rectView)
+        
+        NSLayoutConstraint.activate([
+            rectView.widthAnchor.constraint(equalToConstant: 100),
+            rectView.heightAnchor.constraint(equalToConstant: 100),
+            rectView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            rectView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100)
+        ])
+    }
+}
+
+
+class gradientView: UIView {
+    
+    let gradient = CAGradientLayer()
+
+    init(colors: [CGColor]){
+        super.init(frame: .zero)
+        gradient.frame = bounds
+        gradient.colors = colors
+        layer.addSublayer(gradient)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradient.frame = bounds
+    }
+    
+}
