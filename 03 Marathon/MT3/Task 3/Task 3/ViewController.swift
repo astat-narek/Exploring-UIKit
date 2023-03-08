7928bec546899cbb2b38e683e66699f70a8a6a76
Narek Danielian <astat.narek@gmail.com>
2023-03-08 14:25:44 +0300

    added layoutMargins and animate

--- 03 Marathon/MT3/Task 3/Task 3/ViewController.swift
+++ 03 Marathon/MT3/Task 3/Task 3/ViewController.swift
@@ -8,31 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     let slider: UISlider = UISlider()
-    
-    
-    
     let rect: UIView = UIView()
+    
+    var margin: CGFloat {
+        view.layoutMargins.left
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.layoutMargins = view.safeAreaInsets
         
         style()
         layout()
     }
+    
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
 
+        getNewPosition(for: rect, at: slider.value)
+    }
     
     
     func style() {
-        rect.frame = CGRect(x: 20, y: 150, width: 100, height: 100)
+        rect.frame = CGRect(x: margin, y: 150, width: 100, height: 100)
         rect.backgroundColor = .systemBlue
         rect.layer.cornerRadius = 20
         slider.addTarget(self, action: #selector(self.sliderValueDidChange(_:event:)), for: .valueChanged)
-
     }
+    
     
     func layout() {
         view.addSubview(slider)
@@ -43,43 +51,32 @@ class ViewController: UIViewController {
             slider.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor),
             slider.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor),
             slider.topAnchor.constraint(equalTo: view.topAnchor, constant: 300)
-            
-            
         ])
     }
     
+    
     @objc func sliderValueDidChange(_ sender: UISlider, event: UIEvent) {
         
-        let length = view.frame.width-rect.frame.width-40
-        
-        
-        if let touchEvent = event.allTouches?.first {
-                switch touchEvent.phase {
-                case .began:
-                    print("began")
-                case .moved:
-                    print("moved: \(slider.value)")
-                    let degrees = 90*CGFloat(slider.value)
-                    let scale = 1+(0.5*CGFloat(slider.value))
-                    rect.transform = CGAffineTransformMakeRotation(degrees * .pi/180).scaledBy(x: scale, y: scale)
-
-                    rect.layer.position.x = CGFloat(slider.value)*(length)+rect.frame.width/2+20
-                case .ended:
-                    
-                    UIView.animate(withDuration: 0.1, delay: 0) { [self] in
-                        slider.setValue(1, animated: false)
-                        print(slider.value)
-                        let degrees = 90*CGFloat(slider.value)
-                        let scale = 1+(0.5*CGFloat(slider.value))
-                        rect.transform = CGAffineTransformMakeRotation(degrees * .pi/180).scaledBy(x: scale, y: scale)
-
-                        rect.layer.position.x = view.frame.width-rect.frame.width/2-20
-                    }
-                    
-                default:
-                    break
-                }
+        if event.allTouches?.first?.phase == .ended {
+            UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut]) { [self] in
+                slider.setValue(1, animated: true)
+                getNewPosition(for: rect, at: slider.value)
             }
+        } else {
+            getNewPosition(for: rect, at: slider.value)
+        }
+    }
+    
+    
+    func getNewPosition(for rect: UIView, at sliderValue: Float) {
+        
+        let sliderValue = CGFloat(sliderValue)
+        
+        let degrees = 90 * sliderValue
+        let scale = 1 + (0.5 * sliderValue)
+        rect.transform = CGAffineTransformMakeRotation(degrees * .pi / 180).scaledBy(x: scale, y: scale)
+        let length = view.frame.width - rect.frame.width - 2 * margin
+        rect.layer.position.x = length * sliderValue + rect.frame.width / 2 + margin
     }
 }
 
