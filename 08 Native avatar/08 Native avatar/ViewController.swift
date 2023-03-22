e9b6a8793c0ba143f87b791c8bfacd7d595614e9
Narek Danielian <astat.narek@gmail.com>
2023-03-22 20:01:44 +0300

    initial commit

---
+++ 08 Native avatar/08 Native avatar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  08 Native avatar
+//
+//  Created by Narek Danielyan on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
