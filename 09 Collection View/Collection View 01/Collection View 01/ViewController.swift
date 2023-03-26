75dcf0a4aec24939d001559d0da1b725066ca562
Narek Danielian <astat.narek@gmail.com>
2023-03-26 09:36:54 +0300

    initial commit

---
+++ 09 Collection View/Collection View 01/Collection View 01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection View 01
+//
+//  Created by Narek Danielyan on 26.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
