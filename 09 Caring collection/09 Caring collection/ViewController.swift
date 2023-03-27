b433bac6f267b4ead9db6dcbbf6d8f11f57a7c87
Narek Danielian <astat.narek@gmail.com>
2023-03-27 22:29:18 +0300

    refactoring collection view

--- 09 Caring collection/09 Caring collection/ViewController.swift
+++ 09 Caring collection/09 Caring collection/ViewController.swift
@@ -7,19 +7,13 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
     let data = Array(0...10)
-
-    lazy var collectionView: UICollectionView = {
-        let layout = SnappingFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        layout.scrollDirection = .horizontal
-        let collectionView = UICollectionView(frame: CGRect.zero,
-                                              collectionViewLayout: layout)
-        collectionView.backgroundColor = .systemBackground
-        return collectionView
-    }()
+    
+    lazy var collectionView: UICollectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: SnappingFlowLayout())
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,55 +24,59 @@ class ViewController: UIViewController {
         collectionView.frame = view.frame
         view.addSubview(collectionView)
         collectionView.register(RectCell.self, forCellWithReuseIdentifier: RectCell.reuseIdentifier)
+        collectionView.preservesSuperviewLayoutMargins = true
         collectionView.dataSource = self
         collectionView.delegate = self
     }
-    
-    
 }
 
 
 class SnappingFlowLayout: UICollectionViewFlowLayout {
     
+    override init() {
+        super.init()
+        scrollDirection = .horizontal
+        itemSize = . init(width: 280, height: 400)
+        sectionInsetReference = .fromLayoutMargins
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-            guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
-
-            var offsetAdjustment = CGFloat.greatestFiniteMagnitude
-        let horizontalOffset = proposedContentOffset.x + sectionInset.left
-
-            let targetRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
-
-            let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
-
-            layoutAttributesArray?.forEach({ (layoutAttributes) in
-                let itemOffset = layoutAttributes.frame.origin.x
-                if fabsf(Float(itemOffset - horizontalOffset)) < fabsf(Float(offsetAdjustment)) {
-                    offsetAdjustment = itemOffset - horizontalOffset
-                }
-            })
-            
-        offsetAdjustment = offsetAdjustment - 10
+        guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
         
-            return CGPoint(x: proposedContentOffset.x + offsetAdjustment, y: proposedContentOffset.y)
+        let horizontalOffset = proposedContentOffset.x
+        
+        let targetRect = CGRect.init(origin: .init(x: proposedContentOffset.x, y: 0), size: collectionView.bounds.size)
+        var offsetAdjustment = CGFloat.greatestFiniteMagnitude
+        
+        for layoutAttributes in super.layoutAttributesForElements(in: targetRect)! {
+            let itemOffset = layoutAttributes.frame.origin.x
+            if (abs(itemOffset - horizontalOffset) < abs(offsetAdjustment)) {
+                offsetAdjustment = itemOffset - horizontalOffset
+            }
         }
+        return CGPoint.init(x: proposedContentOffset.x + offsetAdjustment - collectionView.layoutMargins.left, y: proposedContentOffset.y)
+    }
+    
 }
-
-
-
 
 
 // MARK: - Collection View Delegate
 extension ViewController: UICollectionViewDelegateFlowLayout {
-        
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         CGSize(width: 300, height: 400)
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         20
     }
-
 }
 
 // MARK: - Collection View Data Source
@@ -88,19 +86,18 @@ extension ViewController: UICollectionViewDataSource {
         1
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         data.count
     }
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RectCell.reuseIdentifier, for: indexPath) as? RectCell else {
             fatalError("Cell cannot be created")
         }
-        
         return cell
     }
-    
-    
 }
 
 
